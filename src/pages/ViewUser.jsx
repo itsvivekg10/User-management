cab7ea0896fc864c69377abf7ead19a509c57823
vivek gupta <itsvivekg10@gmail.com>
2025-11-18 23:26:45 +0530

    name error

--- src/pages/ViewUser.jsx
+++ src/pages/ViewUser.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ref, onValue } from "firebase/database";
-import { database } from "../firebaseConfig";
+ import { database } from "../fireBaseConfig";
+
 import UserForm from "../components/UserForm";
 
 const ViewUser = () => {
